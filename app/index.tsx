06754d6445842d570948dcab2f68fcc27babf48c
phaothu2002x <truongthuan21342@gmail.com>
2025-07-30 16:10:26 +0700

    homescreen

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,87 @@
-import { Text, View } from 'react-native';
-
+import CartButton from '@/components/CartButton';
+import { images, offers } from '@/constants';
+import { Fragment } from 'react';
+import {
+    FlatList,
+    Image,
+    Pressable,
+    Text,
+    TouchableOpacity,
+    View,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 export default function Index() {
     return (
-        <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-3xl font-quicksand-semibold text-primary">
-                Welcome to Nativewind!
-            </Text>
-        </View>
+        <SafeAreaView className="flex-1 bg-white">
+            <FlatList
+                data={offers}
+                renderItem={({ item, index }) => {
+                    //checking card even
+
+                    const isEven = index % 2 === 0;
+
+                    return (
+                        <View>
+                            <Pressable
+                                // className={cn(
+                                //     'offer-card',
+                                //     isEven ? 'flex-row-reverse' : 'flex-row'
+                                // )}
+                                className={`offer-card ${isEven ? 'flex-row-reverse' : 'flex-row'}`}
+                                style={{ backgroundColor: item.color }}
+                                android_ripple={{ color: '#ffff22' }}
+                            >
+                                {({ pressed }) => (
+                                    <Fragment>
+                                        <View className="h-full w-1/2">
+                                            <Image
+                                                source={item.image}
+                                                className="size-full"
+                                                resizeMode="contain"
+                                            />
+                                        </View>
+                                        <View
+                                            className={`offer-card__info ${isEven ? 'pl-10' : 'pr-10'}`}
+                                        >
+                                            <Text className="h1-bold text-white leading-tight">
+                                                {item.title}
+                                            </Text>
+                                            <Image
+                                                source={images.arrowRight}
+                                                className="size-10"
+                                                tintColor="ffffff"
+                                                resizeMode="contain"
+                                            />
+                                        </View>
+                                    </Fragment>
+                                )}
+                            </Pressable>
+                        </View>
+                    );
+                }}
+                contentContainerClassName="pb-28 px-5"
+                ListHeaderComponent={() => (
+                    <View className="flex-between flex-row w-full my-5 px-5">
+                        <View className="flex-start">
+                            <Text className="small-bold text-primary">
+                                Delivery To
+                            </Text>
+
+                            <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
+                                <Text className="text-dark-100 paragraph-bold ">
+                                    Viet Nam
+                                </Text>
+                                <Image
+                                    source={images.arrowDown}
+                                    className="size-3 "
+                                    resizeMode="contain"
+                                />
+                            </TouchableOpacity>
+                        </View>
+                        <CartButton />
+                    </View>
+                )}
+            />
+        </SafeAreaView>
     );
 }
